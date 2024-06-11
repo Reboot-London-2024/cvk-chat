9f97b89500e0c1e4418bcab8f09ae54bf6399273
Rohith <rohithm8@googlemail.com>
2024-06-11 13:33:18 +0100

    changes

--- app.py
+++ app.py
@@ -1,28 +1,43 @@
 from flask import Flask, render_template, request, jsonify
 import vertexai
-from vertexai.language_models import ChatModel
+from vertexai.generative_models import GenerativeModel
 import os
 
 app = Flask(__name__)
-PROJECT_ID = "tt-dev-001"  
-LOCATION = "us-central1"  
+PROJECT_ID = "lbplc-reboot24lon-974"  
+LOCATION = "europe-west2"  
 
 vertexai.init(project=PROJECT_ID, location=LOCATION)
 
 def create_session():
-    chat_model = ChatModel.from_pretrained("chat-bison@001")
-    chat = chat_model.start_chat()
-    return chat
+    model = GenerativeModel.from_pretrained(
+        model_name="gemini-1.5-pro-001",
+        system_instruction=[
+            "You are a chatbot for Lloyds Bank, helping customers to set up trusted roles for their banking accounts. There are a range of these roles, described below:",
+            """
+Temporary account control: Full account control for a limited period.
+Temporary payments access: Can make payments for a set period.
+Palliative care service: Manages finances for medical and daily care needs.
+Dementia care service: Oversees transactions and bill payments for someone with dementia.
+Transaction double checking: Approves all transactions before completion.
+Limited cash card access: Restricted access to withdraw funds or make purchases up to a limit.""",
+            "You will follow these steps: Firstly, ask the customer for the nominee's first name, surname, email, phone number and address.",
+            "Next, you will ask them to describe the problem and what accesses they would like to grant. You will suggest one of the roles above.",
+            "Finally, you will ask them which revalidation period they would like: 3, 6 or 12 months."
+        ]
+        )
+    return model.start_chat()
+
 
 def response(chat, message):
     parameters = {
         "temperature": 0.2,
-        "max_output_tokens": 256,
-        "top_p": 0.8,
-        "top_k": 40
     }
-    result = chat.send_message(message, **parameters)
-    return result.text
+    text_response = []
+    responses = chat.send_message(message, stream=True)
+    for chunk in responses:
+        text_response.append(chunk.text)
+    return "".join(text_response)
 
 @app.route('/')
 def index():
